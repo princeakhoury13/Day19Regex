16fea026dbcc71af3c8b50145968d3452176f7f3
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-02 11:34:40 +0530

    added: Check for valid last name

--- Program.cs
+++ Program.cs
@@ -6,17 +6,17 @@
         {
             Patterns pattern = new Patterns();
 
-            Console.WriteLine("Enter your Name: ");
+            Console.WriteLine("Enter your Last Name: ");
             string name = Console.ReadLine();
             bool val = pattern.validate(name);
 
             if(val)
             {
-                Console.WriteLine("Valid First Name");
+                Console.WriteLine("Valid Last Name");
             }
             else
             {
-                Console.WriteLine("Invalid First Name!");
+                Console.WriteLine("Invalid Last Name!");
             }
         }
     }
