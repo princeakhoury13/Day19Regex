2a98eb0580cbe5989dda2514afbe7a1ec9bfe0af
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-02 11:30:52 +0530

    added: Check for valid name

--- Program.cs
+++ Program.cs
@@ -4,7 +4,20 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the master branch for Assignment --> Day 19 Regex");
+            Patterns pattern = new Patterns();
+
+            Console.WriteLine("Enter your Name: ");
+            string name = Console.ReadLine();
+            bool val = pattern.validate(name);
+
+            if(val)
+            {
+                Console.WriteLine("Valid First Name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid First Name!");
+            }
         }
     }
 }
