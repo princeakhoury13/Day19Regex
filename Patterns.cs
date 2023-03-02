aa0b563fd62b070e48aa3accc66350c1d75fe55d
princeakhoury13 <princepraveen130509@gmail.com>
2023-03-02 11:31:16 +0530

    added: Regex for given name condition

---
+++ Patterns.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Day19Regex
+{
+    public class Patterns
+    {
+        public static string REGEX_PIN = "^[A-Z]{1}[a-z]{2,}$";
+
+        public bool validate(string name)
+        {
+            return Regex.IsMatch(name, REGEX_PIN);
+        }
+    }
+}
